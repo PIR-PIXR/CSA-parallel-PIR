4569f178ccbf2264a3abf3ca01188350c355d231
QUANG <caoquang91@gmail.com>
2023-05-26 17:32:07 +0300

    Update figurePIR.py

--- parallel-PIR/graphs/figurePIR.py
+++ parallel-PIR/graphs/figurePIR.py
@@ -348,7 +348,7 @@ fig, a = plt.subplots(figsize=(7, 5))
 # Plotting Server costs
 a.plot(num_leave, Avg_WholeTree_Comp_Server, label='h-time-WholeTree', marker='x', linestyle='-', color='black')
 a.plot(num_leave, Avg_OneProof_Comp_Server, label='Proof-as-Element', marker='.', linestyle='-.', color='black')
-a.plot(num_leave, Avg_Layer_Comp_Server, label='Layer-based', marker='x', linestyle=':', color='black')
+a.plot(num_leave, Avg_Layer_Comp_Server, label='Layer-based', marker='.', linestyle=':', color='black')
 a.plot(num_leave, Avg_PBC_Comp_Server, label='Batch-SealPIR', marker='x', linestyle='--', color='black')
 a.plot(num_leave, Avg_Color_Comp_Server, label='Coloring-based', marker='.', linestyle='-', color='black')
 
@@ -369,9 +369,9 @@ Avg_PBC_Comp_Server_zoom = Avg_PBC_Comp_Server[8:11]
 Avg_Color_Comp_Server_zoom = Avg_Color_Comp_Server[8:11]
 
 # Plotting Client costs
-in_ax.plot(num_leave_zoom, Avg_Layer_Comp_Server_zoom, label='Layer-based', marker='x', linestyle=':', color='black')
+in_ax.plot(num_leave_zoom, Avg_Layer_Comp_Server_zoom, label='Layer-based', marker='.', linestyle=':', color='black')
 in_ax.plot(num_leave_zoom, Avg_PBC_Comp_Server_zoom, label='Batch-SealPIR', marker='x', linestyle='--', color='black')
-in_ax.plot(num_leave_zoom, Avg_Color_Comp_Server_zoom, label='Coloring-based', marker='o', linestyle='-', color='black')
+in_ax.plot(num_leave_zoom, Avg_Color_Comp_Server_zoom, label='Coloring-based', marker='.', linestyle='-', color='black')
 
 # Chart customization
 in_ax.set_xlabel('n', weight='bold', size = 8)
